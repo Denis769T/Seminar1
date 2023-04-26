f546e85c9b937c27e95a6473d67c5d13a917cf97
Denis <denis-topalov@yandex.ru>
2023-04-26 19:55:39 +1100

    Seminar 1. Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b & a > c) 
+{
+    Console.Write($"Первое число является максимльным->{a}");
+}
+if (b > a & b > c) 
+{
+    Console.Write($"Второе число является максимльным->{b}");
+}
+if (c > a & c > b) 
+{
+    Console.Write($"Третье число является максимльным->{c}");
+}
+if (a == b & a == c & b ==c)
+{
+    Console.Write("Error!");
+}
+
